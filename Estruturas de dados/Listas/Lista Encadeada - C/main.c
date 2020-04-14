4da634f73d4224a229e2b39797be33ef203aa7f0
jkevinx23 <kevingomesr@gmail.com>
2020-04-14 12:57:28 -0300

    Lista dinâmica encadeada - insert/exchange

--- Estruturas de dados/Listas/Lista Encadeada - C/main.c
+++ Estruturas de dados/Listas/Lista Encadeada - C/main.c
@@ -24,6 +24,9 @@ void pop(List* list);
 bool isEmpty(List* list);
 Node* atPos(List* list, int index);
 int indexOf(List* list, Node* node);
+void erase(List* list, int index);
+void insert(List* list, int index, DataNode data);
+void exchangeNodes(List* list, Node* nodeA, Node* nodeB);
 
 List* createList(){
 
@@ -50,8 +53,8 @@ void printList(List* list){
         printf("Empty List\n");
         return;
     }
-
-     Node* pointer = list -> head;
+    printf("Size -> %d\n", list->size);
+    Node* pointer = list -> head;
 
     while(pointer != NULL){
         printf("%d ", pointer->data.id);
@@ -79,11 +82,10 @@ bool isEmpty(List* list){
 
 Node* atPos(List* list, int index){
     
-    if(index<0 || index > list->size){
+    if(index < 0 || index >= list->size){
         printf("Index inválido");
         return NULL;
     }
-
     Node* node = list->head;
     int i;
     for (i=0;i<index;i++){
@@ -107,17 +109,105 @@ int indexOf(List* list, Node* node){
     return -1;
 }
 
+void erase(List* list, int index){
+    if(list->size == 0 ){
+        printf("The list not have nodes");
+        return;
+    }
+    if(index == 0){
+        Node* aux = list->head;
+        list->head = list->head->next;
+        list->size--;
+        free(aux);
+        printList(list);
+        return;
+    }
+
+    Node* pointer = atPos(list,index-1);
+    if(pointer->next!= NULL){
+        Node* node = pointer->next;
+        pointer->next = node->next;
+        list->size--;
+        free(node);
+        printList(list);  
+        return;
+    }
+    printf("Index invalid");
+    return;
+}
+
+void insert(List* list, int index, DataNode data){
+    
+    if(index<0 || index>list->size){
+        printf("Invalid index");
+        return;
+    }
+
+    if(index == 0){
+        push(list, data);
+        return;
+    }
+    Node* node = (Node*) malloc(sizeof(Node));
+    node->data = data;
+
+    Node* aux = atPos(list, index-1);
+    node->next = aux->next;
+    aux -> next = node;
+    list->size++;
+    printList(list);
+}
+
+void exchangeNodes(List* list, Node* nodeA, Node* nodeB){
+    
+    if(nodeA == nodeB){
+        return;
+    }
+
+    int indexA = indexOf(list, nodeA);
+    int indexB = indexOf(list, nodeB);
+    
+    if( indexA > indexB ){
+        nodeA = nodeB;
+        nodeB = atPos(list, indexA);
+
+        indexA = indexB;
+        indexB = indexOf(list, nodeB); 
+    }
+
+    Node* previewB = atPos(list, indexB-1);
+    
+    if(nodeA == list->head){
+        list->head = nodeB;
+    }
+    else{
+        Node* previewA = atPos(list, indexA-1);
+        previewA -> next = nodeB;
+    }
+    previewB -> next = nodeA;
+
+    Node* pointer = nodeA->next;
+
+    nodeA->next = nodeB->next;
+
+    nodeB->next = pointer;
+
+    printList(list);
+}
+
 int main(){
     List* lista = createList();
-    
     DataNode data;
     data.id  = 5;
     push(lista, data);
     data.id  = 10;
     push(lista, data);
+    data.id  = 8;
+    push(lista, data);
+    data.id  = 17;
+    push(lista, data);
     printList(lista);
+    data.id = 33;
+    insert(lista, 4 ,data);
 
-    printf("%d", indexOf(lista, lista->head->next));
-    
     return 0;
 }
